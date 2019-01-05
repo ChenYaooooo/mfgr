f9d7aa74840f96b99b672f7e03ac0812df941158
Yao <m15229204898@16.com>
2019-01-05 20:10:29 +0800

    轮播图  swiper

--- src/app/views/card/dashboard-swiper/dashboard-swiper.component.ts
+++ src/app/views/card/dashboard-swiper/dashboard-swiper.component.ts
@@ -1,15 +1,31 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
     selector: 'app-dashboard-swiper',
     templateUrl: './dashboard-swiper.component.html',
     styleUrls: ['./dashboard-swiper.component.css']
 })
-export class DashboardSwiperComponent implements OnInit {
+export class DashboardSwiperComponent implements AfterViewInit {
+    testSwiper: Swiper;
 
     constructor() { }
 
-    ngOnInit() {
+    ngAfterViewInit() {
+      var swiperH = new Swiper('.swiper-container-h', {
+        spaceBetween: 50,
+        pagination: {
+          el: '.swiper-pagination-h',
+          clickable: true,
+        },
+      });
+      var swiperV = new Swiper('.swiper-container-v', {
+        direction: 'vertical',
+        spaceBetween: 50,
+        pagination: {
+          el: '.swiper-pagination-v',
+          clickable: true,
+        },
+      });
     }
-
 }
