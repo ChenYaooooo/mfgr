d20469d7b467a61add092742b52c60d65c00876f
Yao <m15229204898@16.com>
2019-01-08 20:48:39 +0800

     解决了setInterval定时器的问题

--- src/app/views/image-auto-play/image-auto-play.component.ts
+++ src/app/views/image-auto-play/image-auto-play.component.ts
@@ -1,32 +1,41 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit , OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-image-auto-play',
   templateUrl: './image-auto-play.component.html',
   styleUrls: ['./image-auto-play.component.css']
 })
-export class ImageAutoPlayComponent implements AfterViewInit {
-
+export class ImageAutoPlayComponent implements AfterViewInit, OnDestroy {
+  timer: any;
   constructor() { }
 
   // ngOnInit() {
   //   this.playImage();
   // }
   ngAfterViewInit() {
-    this.playImage();
+    this.routerOnActivate();
   }
 
-  playImage() {
-    var leftNum = -1800;
-      // setTimeout(document.getElementById('image-gallery').style.left = leftNum + 'px', 3000);
-      window.setInterval(function () {
-        leftNum = leftNum - 1;
-        document.getElementById('image-gallery').style.left = leftNum + 'px';
-        console.log(leftNum);
-      if (leftNum > 0) {
-        leftNum = -1800;
-      }
-      }, 100); // repeat forever, polling every 3 seconds
-
+  ngOnDestroy() {
+    this.routerOnDeactivate();
   }
+
+routerOnActivate() {
+  console.log("true");
+  var leftNum = -1800;
+  this.timer = setInterval(() => {
+    leftNum = leftNum - 1;
+    document.getElementById('image-gallery').style.left = leftNum + 'px';
+    console.log(leftNum);
+    if (leftNum > 0) {
+      leftNum = -1800;
+    }
+  }, 400);
+}
+
+
+routerOnDeactivate() {
+  console.log("false");
+  clearInterval(this.timer);
+}
 }
