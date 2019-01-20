b6ac60cc163e6c6974fb9ead1c6bfc5599951445
Yao <m15229204898@16.com>
2019-01-20 09:07:29 +0800

    box shadow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'mfgr';
+  showHeader = true;
+  constructor(private router: Router) { }
+  ngOnInit() {
+    this.router.events.subscribe(
+      event => {
+        if (event instanceof NavigationEnd) {
+          console.log(event.url);
+        }
+      }
+    );
+  }
 }
--- src/app/views/header1/header1.component.ts
+++ src/app/views/header1/header1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header1',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Header1Component implements OnInit {
 
-  constructor() { }
+  currentUrl: String = '';
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
+    this.currentUrl = this.router.url;
+    console.log(this.router.url);
+    this.router.events.subscribe(
+      event => {
+        if (event instanceof NavigationEnd) {
+          this.currentUrl = event.url;
+          console.log(this.currentUrl);
+        }
+      }
+    );
   }
 
 }
